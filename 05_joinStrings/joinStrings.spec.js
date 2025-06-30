522cac58673e4dd9fd38b361993cc32177f8d73a
drheaa <aucharazrhea@gmail.com>
2025-06-30 16:13:46 +0400

    Edited tests for the optimised variables

--- 05_joinStrings/joinStrings.spec.js
+++ 05_joinStrings/joinStrings.spec.js
@@ -1,28 +1,28 @@
 const values = require('./joinStrings')
 
 describe('step 2', () => {
-  test('firstName is Rhea', () => {
+  test.skip('firstName is Rhea', () => {
     expect(values.firstName).toEqual('Rhea');
   });
-  test('lastName is Aucharaz', () => {
+  test.skip('lastName is Aucharaz', () => {
     expect(values.lastName).toEqual('Aucharaz');
   });
-  test('thisYear is 2025', () => {
+  test.skip('thisYear is 2025', () => {
     expect(values.thisYear).toEqual(2025);
   });
-  test('birthYear is 2003', () => {
+  test.skip('birthYear is 2003', () => {
     expect(values.birthYear).toEqual(2003);
   });
-  test('greeting is properly output', () => {
+  test.skip('greeting is properly output', () => {
     expect(values.greeting).toEqual('Hi, how are you? My name is Rhea Aucharaz and I am 22 years old.');
   });
 });
 
 describe('step 3', () => {
-  test.skip('fullName is Rhea Aucharaz', () => {
+  test('fullName is Rhea Aucharaz', () => {
     expect(values.fullName).toEqual('Rhea Aucharaz');
   });
-  test.skip('age is 22', () => {
+  test('age is 22', () => {
     expect(values.age).toEqual(22);
   });
 });
